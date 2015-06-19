b084f3f41ac983d721c3b9eb021460f701814d53
Max Grechko <max@grechko.org>
2015-06-19 11:57:47 +0300

    Remove the inverse association

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,12 +1,7 @@
 class Address < ActiveRecord::Base
   belongs_to :country
 
-  has_many :billing_orders, class_name: 'Order',
-    foreign_key: 'billing_address_id'
-  has_many :shipping_orders, class_name: 'Order',
-    foreign_key: 'shipping_address_id'
-
   validates :address, :zipcode, :city, :phone, presence: true
   validates :zipcode, length: { is: 5 }
-  validates :phone, length: { minimum: 5, maximum: 12}
+  validates :phone, length: { minimum: 5, maximum: 12 }
 end
